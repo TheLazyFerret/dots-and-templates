8aaf2340dfe26fa89cd0fc62e042c8f9dab74c22
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-07-30 16:54:35 +0100

    fix little error cpp

--- cpp/header_comment.hpp
+++ cpp/header_comment.hpp
@@ -1,7 +1,7 @@
 /**
  * Author: TheLazyFerret (https://github.com/TheLazyFerret)
  * Copyright (c) [YEAR] TheLazyFerret
- * Licensed under [LICENSE] MIT License. See LICENSE file in the project root for full license information.
+ * Licensed under [LICENSE] License. See LICENSE file in the project root for full license information.
  * 
  * [BRIEF]
  */
