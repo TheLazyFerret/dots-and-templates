b6b781f6f7503445e934b58204a4741b5620063f
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-07-31 18:49:51 +0100

    updated c++ header comment

--- cpp/header_comment.hpp
+++ cpp/header_comment.hpp
@@ -1,7 +1,8 @@
 /**
- * Author: TheLazyFerret (https://github.com/TheLazyFerret)
- * Copyright (c) [YEAR] TheLazyFerret
- * Licensed under [LICENSE] License. See LICENSE file in the project root for full license information.
+ * @file [FILE NAME]
+ * @author TheLazyFerret (https://github.com/TheLazyFerret)
+ * @copyright (c) [YEAR] TheLazyFerret
+ *  Licensed under [LICENSE] License. See LICENSE file in the project root for full license information.
  * 
- * [BRIEF]
+ * @brief [BRIEF]
  */
