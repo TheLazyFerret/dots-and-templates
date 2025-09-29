758528d37f4e57b56533669b63ef93a42106f09d
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-09-29 10:28:32 +0000

    update c++ header file

--- cpp/header_comment.hpp
+++ cpp/header_comment.hpp
@@ -1,5 +1,6 @@
 //! Author [AUTHOR NAME] (OPTIONAL AUTHOR SOCIALS)
 //! Copyright (c) [YEAR] [AUTHOR NAME]
-//!   License [LICENSE] license. See LICENSE file in the project root for full license information.
+//!   License [LICENSE] license. 
+//!   See the LICENSE file in the project root for full license information.
 //!
 //! [BRIEF]
