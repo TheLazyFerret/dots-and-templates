20df00ec52c43b35a70b1d8c5b2c38bc8024c64c
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-10-01 20:35:00 +0000

    update setup.sh

--- linux/silverblue/setup.sh
+++ linux/silverblue/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# For all functions: 
+#   0 -> true 
+#   1 -> false
+
 is_ostree_busy() {
   while true; do
     state=$(rpm-ostree status | grep -i state | awk '{print $1}')
@@ -7,14 +11,15 @@ is_ostree_busy() {
       echo "rpm-ostree is busy, waiting"
       sleep 10
     else
-      return
+      return 0
     fi
   done
 }
 
 is_package_layered() {
   layered_packages=$(rpm-ostree status | grep -i LayeredPackages | head -n 1 | awk '{for (i = 2; i <= NF; ++i) printf "%s ", $i} END {print""}')
-  for package in "$layered_packages"; do
+  for package in $layered_packages; do
+    echo $package
     if [ "$1" = "$package" ]; then # Found a coincidence
       return 0
     fi
@@ -24,8 +29,38 @@ is_package_layered() {
 
 is_package_overrided() {
   overrided_packages=$(rpm-ostree status | grep -i RemovedBasePackages | head -n 1 | awk '{for (i = 2; i <= NF; ++i) printf "%s ", $i} END {print""}')
-  for package in "$overrided_packages"; do
-    if [ "$i" = "$package" ]; then # Found a coincidence
+  for package in $overrided_packages; do
+    if [ "$1" = "$package" ]; then # Found a coincidence
+      return 0
+    fi
+  done
+  return 1
+}
+
+is_ref_installed() { # ref # package
+  installed_refs=$(flatpak list --columns=ref,origin | grep flathub | awk '{print $1}')
+  for ref in $installed_refs; do
+    if [ "$1" = "$ref" ]; then # Found a coincidence
+      return 0
+    fi
+  done
+  return 1
+}
+
+is_remote_enabled() {
+  active_remotes=$(flatpak remotes | awk '{print $1}')
+  for remote in $active_remotes; do
+    if [ "$1" = "$remote" ]; then # Found a coincidence
+      return 0
+    fi
+  done
+  return 1
+}
+
+is_remote_added() {
+  added_remotes=$(flatpak remotes --show-disabled  | awk '{print $1}')
+  for remote in $added_remotes; do
+    if [ "$1" = "$remote" ]; then # Found a coincidence
       return 0
     fi
   done
