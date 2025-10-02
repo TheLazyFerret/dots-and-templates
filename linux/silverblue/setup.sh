b9cbf32c99ded33f196cd0d366db93c9deb21a52
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-10-02 08:32:38 +0000

    update setup.sh

--- linux/silverblue/setup.sh
+++ linux/silverblue/setup.sh
@@ -11,7 +11,7 @@ OVERRIDE_PACKAGES="firefox firefox-langpacks"
 ### FUNCTIONS
 wait_ostree_busy() {
   while true; do
-    state=$(rpm-ostree status | grep -i state | awk '{print $1}')
+    state=$(rpm-ostree status | grep -i state | awk '{print $2}')
     if [ "$state" = "busy" ]; then
       echo "rpm-ostree is busy, waiting"
       sleep 10
@@ -24,7 +24,6 @@ wait_ostree_busy() {
 is_package_layered() {
   layered_packages=$(rpm-ostree status | grep -i LayeredPackages | head -n 1 | awk '{for (i = 2; i <= NF; ++i) printf "%s ", $i} END {print""}')
   for package in $layered_packages; do
-    echo $package
     if [ "$1" = "$package" ]; then # Found a coincidence
       return 0
     fi
@@ -81,9 +80,19 @@ update_ostree() {
 }
 
 install_layers_ostree() {
+  aux=""
+  for package in $LAYER_PACKAGES; do
+    if ! is_package_layered "$package"; then
+      aux="$aux $package"
+    fi
+  done
+  if [ -z "$aux" ]; then
+    echo "Not packages to install"
+    return 0
+  fi
   wait_ostree_busy
   echo -n "Installing layered packages..."
-  if ! rpm-ostree install "$LAYER_PACKAGES" > /dev/null 2>&1; then
+  if ! rpm-ostree install "$aux" > /dev/null 2>&1; then
     echo " Found an error!"
     return 1
   fi
@@ -92,6 +101,16 @@ install_layers_ostree() {
 }
 
 remove_overrides_ostree() {
+  aux=""
+  for package in $OVERRIDE_PACKAGES; do
+    if ! is_package_overrided "$package"; then
+      aux="$aux $package"
+    fi
+  done
+  if [ -z "$aux" ]; then
+    echo "Not packages to remove"
+    return 0
+  fi
   wait_ostree_busy
   echo -n "Removing override packages..."
   if ! rpm-ostree override remove "$OVERRIDE_PACKAGES" > /dev/null 2>&1; then
@@ -100,6 +119,23 @@ remove_overrides_ostree() {
   fi
   echo " Done"
   return 0
+}
+
+uninstall_flatpak_remote() { # remote
+  if ! is_remote_added $1; then
+    echo "The remote is not in the remote list"
+    return 0
+  elif ! is_remote_enabled $1; then
+    echo "The remote is already disabled"
+    return 0
+  fi
+  packages_to_remove=$(flatpak list --columns=ref,origin | grep "$1" | awk '{print $1}')
+  for package in $packages_to_remove; do
+    echo -n "Uninstalling $package..."
+    flatpak uninstall --delete-data --assumeyes "$package" "$1" > /dev/null 2>&1
+    echo " Done"
+  done
+
 }
 
 ### MAIN
