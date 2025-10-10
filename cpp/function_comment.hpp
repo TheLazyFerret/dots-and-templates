e19600b1dc5c943a4f1cf1da5eafaae00e68e95a
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-10-10 10:36:18 +0100

    update function_comment.hpp

--- cpp/function_comment.hpp
+++ cpp/function_comment.hpp
@@ -1,6 +1,6 @@
 /// [BRIEF]
-/// param: [PARAM 1 TYPE]
-/// param: [PARAM 2 TYPE]
-/// param: [PARAM 3 TYPE]
-/// ...
-/// return: [RETURN TYPE]
+///  param:  [PARAM 1 TYPE]
+///  param:  [PARAM 2 TYPE]
+///  param:  [PARAM 3 TYPE]
+///  ...
+///  return: [RETURN TYPE]
