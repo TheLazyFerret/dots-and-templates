6fa689d7ccd3112b605162695db52c06a8b347ee
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-01-20 14:21:51 +0000

    update aliases

--- dots/gentoo/aliases.bash
+++ dots/gentoo/aliases.bash
@@ -4,6 +4,6 @@
 # Source this file to add the aliases to your bash sesion
 
 # System update aliases
-alias sys-update='sudo emaint --auto sync'
+alias sys-update='sudo emerge --sync'
 alias sys-upgrade='sudo emerge --ask --verbose --update --deep --changed-use --getbinpkg @world'
 alias sys-clean='sudo emerge --ask --depclean'
